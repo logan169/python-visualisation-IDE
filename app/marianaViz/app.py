ab63bb13ffa32e8cab573dcbc9908403ceef93a0
logan169 <loganschwartz@hotmail.com>
2017-05-27 20:59:53 +0200

    reformated code

--- app/marianaViz/app.py
+++ app/marianaViz/app.py
@@ -14,30 +14,16 @@ def App():
     @app.route('/')
     def index():
 
-        d = json.dumps([{'x':1, 'y':0, 'z': 1},{'x':2, 'y':0, 'z': 2}])
+        #data
+        d = json.dumps([{'x':1, 'y':0, 'z': 1},{'x':2, 'y':0, 'z': 2},{'x':3, 'y':0, 'z': 1},{'x':4, 'y':0, 'z': 2}])
         print flask.jsonify(d)
-        return flask.render_template("histogram.html", input= d)
-    
-        #return flask.send_from_directory(app.static_folder,"matriceWave.html")
-        #return flask.send_from_directory(app.static_folder,"multiHistogram.html")
-        #return flask.send_from_directory(app.static_folder,"3D_ScatterPlot.html")
-        #return flask.send_from_directory(app.static_folder,"matrice.html")
-        #return flask.send_from_directory(app.static_folder,"lala.html")
-    
-    
-    #return mariana information
-    @app.route('/view/<v>/', methods=['GET'])
-    def changeView(v):
-        
-        print (v) # for testing
 
-        if v == 'matrice':
-            resp = ("<h1>Voila! Platform is ready to used</h1>")
-            
-        return flask.jsonify(**K.JSONResponse(data = resp,error=False,message=''))
+        return flask.render_template("index.html", input= d)
         
-        
-
     return app
+    
+    
+
+    
 
 
